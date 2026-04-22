0402c2d3ace9fefd005a25f6c91893769355fed4
pkanji28 <pkanji28@gmail.com>
2026-04-22 13:04:24 +0100

    Delete css/book.js

--- css/book.js
+++
@@ -1,8 +0,0 @@
-const t=document.getElementById('time');
-for(let i=8;i<=21;i++){
- let h=i%12||12;
- let ap=i<12?'AM':'PM';
- let o=document.createElement('option');
- o.text=h+':00 '+ap;
- t.add(o);
-}

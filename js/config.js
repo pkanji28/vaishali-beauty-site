aba725c591321971b4054381eaa7814baa776880
pkanji28 <pkanji28@gmail.com>
2026-04-23 12:32:39 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,7 @@
 window.SUPABASE_URL = "https://ftcsrejkyfslahhfjkci.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0Y3NyZWpreWZzbGFoaGZqa2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NjIwMTMsImV4cCI6MjA5MjIzODAxM30.z-6LPc9VbpOXTw9zquX8whgm4YGYth0pEdbqe11z0tU";
+window.VB_CONFIG = {
+  primaryNumber: "447599693034",
+  fallbackNumber: "447951077118",
+  businessName: "Vaishali's Beauty"
+};

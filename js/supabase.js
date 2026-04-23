55482d5b1518e7f8544b291e68af7a229456ded8
pkanji28 <pkanji28@gmail.com>
2026-04-23 21:12:01 +0100

    Initialize Supabase client in supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,6 @@
+const supabase = window.supabase.createClient(
+  "https://ftcsrejkyfslahhfjkci.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0Y3NyZWpreWZzbGFoaGZqa2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NjIwMTMsImV4cCI6MjA5MjIzODAxM30.z-6LPc9VbpOXTw9zquX8whgm4YGYth0pEdbqe11z0tU"
+);
+
+window.supabaseClient = supabase;

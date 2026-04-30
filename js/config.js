db5d4ecdcaf1b117887c970dd21be59e1af9787e
pkanji28 <pkanji28@gmail.com>
2026-04-30 23:05:39 +0100

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -2,7 +2,6 @@ window.VB_CONFIG = {
   primaryNumber: "447599693034",
   fallbackNumber: "447951077118",
   businessName: "Vaishali's Beauty",
-
-  supabaseUrl: "https://ftesrejkyfslahhfjkci.supabase.co",
-  supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0Y3NyZWpreWZzbGFoaGZqa2NpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY2NjIwMTMsImV4cCI6MjA5MjIzODAxM30.z-6LPc9VbpOXTw9zquX8whgm4YGYth0pEdbqe11z0tU"
+  supabaseUrl: "YOUR_SUPABASE_URL",
+  supabaseAnonKey: "YOUR_SUPABASE_ANON_KEY"
 };
